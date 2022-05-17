a7f9a6e322e1a245a11bcd46b85cb4fe6e8b3dc8
swc9803 <swc9803@gmail.com>
2022-05-18 00:36:42 +0900

    코드 정리

--- src/pages/Raycaster.vue
+++ src/pages/Raycaster.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="scrollEl" ref="scrollEl" />
-  <div class="container" ref="cont" />
+  <div class="container" ref="container" />
   <section class="section1" />
   <section class="section2" />
   <section class="section3" />
@@ -18,111 +18,96 @@ gsap.registerPlugin(ScrollTrigger)
 
 export default {
   setup () {
-    const cont = ref()
+    const container = ref()
     const scrollEl = ref()
     const color = 0xFFFFFF
     const intensity = 1
+    const fov = 35
+    const near = 0.9
+    const far = 1000
+
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true
+    })
+    const scene = new THREE.Scene()
 
     onMounted(() => {
-      function boxAni () {
-        const fov = 35
-        const aspect = cont.value.clientWidth / cont.value.clientHeight
-        const near = 0.9
-        const far = 1000
-        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-        camera.position.set(0, 0, 2)
+      const width = container.value.clientWidth
+      const height = container.value.clientHeight
 
-        const renderer = new THREE.WebGLRenderer({
-          antialias: true,
-          alpha: true
-        })
-        const scene = new THREE.Scene()
+      // camera
+      const camera = new THREE.PerspectiveCamera(fov, width / height, near, far)
+      camera.position.set(0, 0, 2)
 
-        function init () {
-          renderer.setSize(cont.value.clientWidth, cont.value.clientHeight)
-          renderer.setPixelRatio(window.devicePixelRatio)
+      // light
+      const light = new THREE.DirectionalLight(color, intensity)
+      const light2 = new THREE.DirectionalLight(color, intensity)
+      light2.position.set(0, -5, 2)
+      scene.add(light)
+      scene.add(light2)
 
-          cont.value.appendChild(renderer.domElement)
+      // orbitControls
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.update()
 
-          // eslint-disable-next-line no-unused-vars
-          function render () {
-            renderer.render(scene, camera)
-          }
-          const controls = new OrbitControls(camera, renderer.domElement)
-          controls.update()
+      // raycaster
+      const raycaster = new THREE.Raycaster()
+      const rayOrigin = new THREE.Vector3(-3, 0, 0)
+      const rayDirection = new THREE.Vector3(10, 0, 0)
+      rayDirection.normalize()
+      raycaster.set(rayOrigin, rayDirection)
+      scene.add(new THREE.ArrowHelper(raycaster.ray.direction, raycaster.ray.origin, 300, 0x0000ff))
 
-          const light = new THREE.DirectionalLight(color, intensity)
-          const light2 = new THREE.DirectionalLight(color, intensity)
-          light.position.set(-1, 2, 4)
-          scene.add(light)
-          light2.position.set(1, -2, -4)
-          scene.add(light2)
+      renderer.setSize(width, height)
+      renderer.setPixelRatio(window.devicePixelRatio)
+      container.value.appendChild(renderer.domElement)
 
-          const raycaster = new THREE.Raycaster()
-          const rayOrigin = new THREE.Vector3(-3, 0, 0)
-          const rayDirection = new THREE.Vector3(10, 0, 0)
-          rayDirection.normalize()
-          raycaster.set(rayOrigin, rayDirection)
-          scene.add(new THREE.ArrowHelper(raycaster.ray.direction, raycaster.ray.origin, 300, 0x0000ff))
+      // render
+      renderer.render(scene, camera)
 
-          const geometry = new THREE.BoxGeometry(0.4, 0.2)
-          var material = new THREE.MeshPhongMaterial()
-          var box = new THREE.Mesh(geometry, material)
-          var box2 = new THREE.Mesh(geometry, material)
-          material.color = new THREE.Color(0xff0000)
-          // box.scale.set(1.0, 1.0, 1.0)
-          box.position.set(0, 0, 0)
-          box2.position.set(0, 0, 0)
-          scene.add(box)
-          scene.add(box2)
-          animate()
-          const carAni = gsap.timeline({
-            scrollTrigger: {
-              trigger: scrollEl.value,
-              start: 'top top',
-              end: '80%',
-              scrub: 1
-            }
-          })
-          carAni
-            // .fromTo(cont.value, { opacity: 0.5 }, { opacity: 1 }, '<')
-            // .to(box.rotation, { y: 6.79 }, '<')
-            // .to(box.rotation, { x: 2.6 })
-            // .to(box.rotation, { z: 2.6 }, '<')
-            .to(box2.position, { y: 2 })
-            // .to(camera.rotation, { y: 2 }, '<')
-            // .to(cont.value, { opacity: 0, scale: 0 }, '<')
-          const intersect = raycaster.intersectObjects([box, box2])
-          const log = () => {
-            console.log(intersect)
-          }
-          window.addEventListener('click', log)
-          gsap.to(light.rotation, { y: 20 })
+      // animation
+      const geometry = new THREE.BoxGeometry(0.4, 0.2)
+      var material = new THREE.MeshPhongMaterial()
+      var box = new THREE.Mesh(geometry, material)
+      var box2 = new THREE.Mesh(geometry, material)
+      material.color = new THREE.Color(0xff0000)
+      box2.position.set(0, 0, 0)
+      scene.add(box)
+      scene.add(box2)
+      const carAni = gsap.timeline({
+        scrollTrigger: {
+          trigger: scrollEl.value,
+          start: 'top top',
+          end: '80%',
+          scrub: 1
         }
-
-        function animate () {
-          requestAnimationFrame(animate)
-          renderer.render(scene, camera)
-        }
-        init()
-
-        function onWindowResize () {
-          camera.aspect = cont.value.clientWidth / cont.value.clientHeight
-          camera.updateProjectionMatrix()
-          renderer.setSize(cont.value.clientWidth, cont.value.clientHeight)
-        }
-        window.addEventListener('resize', onWindowResize)
-
-        // scene.rotation.set(0, 1.88, 0)
-
-        // ScrollTrigger.defaults({
-        //   immediateRender: false
-        // })
+      })
+      carAni
+        .to(box2.position, { y: 2 })
+      const intersect = raycaster.intersectObjects([box, box2])
+      const log = () => {
+        console.log(intersect)
       }
-      boxAni()
+      window.addEventListener('click', log)
+      gsap.to(light.rotation, { y: 20 })
+
+      function animate () {
+        requestAnimationFrame(animate)
+        renderer.render(scene, camera)
+      }
+      animate()
+
+      function onWindowResize () {
+        camera.aspect = width / height
+        camera.updateProjectionMatrix()
+        renderer.setSize(width, height)
+      }
+      window.addEventListener('resize', onWindowResize)
     })
+
     return {
-      scrollEl, cont
+      scrollEl, container
     }
   }
 }
